1fbfbace243991365a1191cfb25ad66751104b43
internetghost2 <internetghost2@protonmail.com>
2021-05-12 19:04:07 +0200

    update

--- scripts/AutoGPenT0ols/recon.py
+++ scripts/AutoGPenT0ols/recon.py
@@ -9,29 +9,27 @@ import subprocess
 
 # Vars 
 ## PATH=dsdas
-nmap_p = '{ "name":"nmap allports", "command":"nmap -p- --min-rate 5000 -n -oG ./allports 127.0.0.1"}'
+nmap_p = '{"name":"nmap", "command":["nmap -p- --min-rate 5000 -n -oG ./allports 127.0.0.1", "nmap -p-"]}'
 
 p1 = log.progress("Recon")
 data = json.loads(nmap_p)
 
+
 def main():
-    global single_target
-    global only_scans_dir
-    global port_scan_profile
-    global heartbeat_interval
-    global nmap
-    global srvname
-    global verbose
+    p1.status("{}".format(data['name']))
+    time.sleep(2)
+    with open('out.txt','w+') as fout:
+        with open('err.txt','w+') as ferr:
+            for command in data['command']:
+            out=subprocess.call([command],stdout=fout,stderr=ferr)
+            # reset file to read from it
+            fout.seek(0)
+            # save output (if any) in variable
+            output=fout.read())
 
-    _init()
-    parser = argparse.ArgumentParser(description='Network reconnaissance tool to port scan and automatically enumerate services found on multiple targets.')
-    parser.add_argument('-o', '--output', action='store', default='results', dest='output_dir', help='The output directory for results. Default: %(default)s')
-    parser.add_argument('-t','--target', action='store_true', default=False, help='Only scan a single target. A directory named after the target will not be created. Instead, the directory structure will be created within the output directory. Default: false')
-    parser.error = lambda s: fail(s[0].upper() + s[1:])
-    args = parser.parse_args()
+            # reset file to read from it
+            ferr.seek(0) 
+            # save errors (if any) in variable
+            errors = ferr.read()
 
-    target = args.target
-    only_scans_dir = args.only_scans_dir
-
-    errors = False
-
+main()
