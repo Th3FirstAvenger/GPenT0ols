8785f32d859c0b40e4ac0aee5728135bb4d5e419
Th3FirstAvenger <teamshadowdc@protonmail.com>
2021-05-15 12:14:07 +0200

    update

--- scripts/AutoGPenT0ols/recon.py
+++ scripts/AutoGPenT0ols/recon.py
@@ -8,28 +8,66 @@ import subprocess
 
 
 # Vars 
-## PATH=dsdas
-nmap_p = '{"name":"nmap", "command":["nmap -p- --min-rate 5000 -n -oG ./allports 127.0.0.1", "nmap -p-"]}'
 
-p1 = log.progress("Recon")
-data = json.loads(nmap_p)
+PATH="/tmp/autorecon/"
+
+nmap_p = '{"name":"nmap", "commands":["nmap -p- --min-rate 5000 -n -oG allports 127.0.0.1", "nmap -p-"]}'
 
 
-def main():
-    p1.status("{}".format(data['name']))
-    time.sleep(2)
-    with open('out.txt','w+') as fout:
-        with open('err.txt','w+') as ferr:
-            for command in data['command']:
-            out=subprocess.call([command],stdout=fout,stderr=ferr)
+
+infra = log.progress("Build")
+
+def mdir(dir_name):
+    try:
+        os.makedirs(dir_name)
+    except OSError:
+        infra.failure("Creation of the directory {} failed".format(dir_name))
+    except: 
+        infra.status("Succes")
+    else: 
+        infra.status("Succesfully created the directory {}".format(dir_name))
+    
+
+def run(dir_file, command):
+    out = dir_file + "out.txt"
+    err = dir_file + "err.txt"
+
+    with open(out,'w+') as fout:
+        with open(err,'w+') as ferr:
+            out=subprocess.call(command.split(),stdout=fout,stderr=ferr)
             # reset file to read from it
             fout.seek(0)
             # save output (if any) in variable
-            output=fout.read())
+            output=fout.read()
 
             # reset file to read from it
             ferr.seek(0) 
             # save errors (if any) in variable
             errors = ferr.read()
 
-main()
+
+
+
+def main():
+    
+    mdir(PATH)
+    infra.status("Building structure on {}".format(PATH))
+    
+    data = json.loads(nmap_p)
+    
+    recon = log.progress("Recon")
+    
+    recon.status("Enum with {}".format(data['name']))
+    dir_name = PATH + data['name'] + "/"
+    dir_tmp = "tmp/" + data['name']
+    mdir(dir_name) 
+    mdir(dir_tmp) 
+        
+    for command in data['commands']: 
+
+       run(dir_name,command)
+       time.sleep(2)
+
+
+if __name__ == '__main__':
+    main()
